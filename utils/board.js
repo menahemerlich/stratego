5319b6b0fcea51a15f1847fe35d5722fd3b09bb9
menahem_erlich <erlichmenahem@gmail.com>
2025-12-07 17:30:22 +0200

    creat board and deploy troops

---
+++ utils/board.js
@@ -0,0 +1,26 @@
+
+export function creatBoard(boardSize = 10, fill = '_') {
+    const board = [];
+    for (let i = 0; i < boardSize; i++) {
+        board.push(Array(boardSize).fill(fill))
+    }
+    return board
+}
+
+export function deployTroops(board, displayBoard, soldiers) {
+    let index = null
+    while (soldiers.length > 0) {
+        for (let i = 0; i < board.length; i++) {
+            index = Math.floor(Math.random() * board.length)
+            if (board[board.length -1][index] == '_') {
+                board[board.length -1][index] = soldiers.pop();
+                displayBoard[board.length -1][index] = 'X'
+            }
+        }
+    }
+    index = Math.floor(Math.random() * board.length)
+    board[0][index] = Math.floor(Math.random() * 10)
+    displayBoard[0][index] = 'V'
+}
+
+
