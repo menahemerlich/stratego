4a23666e955a0063a39296041704e6cec8c75c99
DavidPopovich3261 <8534397@gmail.com>
2025-12-08 17:46:10 +0200

    pull marge

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ import { isInBoard, isAssociated, isEmpty, motionLogic, updatePosition } from ".
 import { battle } from "./game/battle.js";
 import { turn } from "./game/turn.js";
 
-const board = creatBoard(10)
+const board = creatBoard(4)
 
 
-const displayBoard = creatBoard(10)
+const displayBoard = creatBoard(4)
 const soldiers = [{ rank: 1 , playerName: 'p1'}, { rank: 2, playerName: 'p1' }, 
-    { rank: 3, playerName: 'p1' }, { rank: 4, playerName: 'p1' }, 
-    { rank: 5, playerName: 'p1' }, { rank: 6, playerName: 'p1' }, 
-    { rank: 7, playerName: 'p1' }, { rank: 8, playerName: 'p1' },
-    { rank: 9, playerName: 'p1' }, { rank: 'flag', playerName: 'p1' }]
+    { rank: 3, playerName: 'p1' }, { rank: 4, playerName: 'p1' } 
+    // { rank: 5, playerName: 'p1' }, { rank: 6, playerName: 'p1' }, 
+    // { rank: 7, playerName: 'p1' }, { rank: 8, playerName: 'p1' },
+    // { rank: 9, playerName: 'p1' }, { rank: 'flag', playerName: 'p1' }
+    ]
     deployTroops(board, displayBoard, soldiers);
 console.table(board);
 console.table(displayBoard);
